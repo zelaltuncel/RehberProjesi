6b7fb0bb1e302d13ed8f483079d3c876bbf3691e
zelaltuncel <zelaltuncel@hotmail.com>
2021-02-13 23:13:59 +0300

    PersonAdd Operation

---
+++ RehberApi/Controllers/PersonController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RehberApi.Models.ORM.Context;
+using RehberApi.Models.ORM.Entities;
+using RehberApi.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RehberApi.Controllers
+{
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DirectoryContext _directoryContext;
+
+        public PersonController(DirectoryContext directoryContext)
+        {
+            _directoryContext = directoryContext;
+        }
+
+        [Route("Person/Add")]
+        [HttpPost]
+
+        public IActionResult Add([FromForm] PersonAddVM personAdd)
+        {
+            if (ModelState.IsValid)
+            {
+                Person person = new Person();
+                person.Name = personAdd.name;
+                person.SurName = personAdd.surName;
+                person.Company = personAdd.company;
+
+                _directoryContext.People.Add(person);
+                _directoryContext.SaveChanges();
+
+                personAdd.id = person.ID;
+
+                return Ok(personAdd);
+
+            }
+            else
+            {
+                return BadRequest(ModelState.Values);
+            }
+        }
+
+        
+    }
+}
---
+++ RehberApi/Migrations/20210213200913_EditTables.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RehberApi.Migrations
+{
+    public partial class EditTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddDate",
+                table: "People",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddDate",
+                table: "ContactInfos",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddDate",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "AddDate",
+                table: "ContactInfos");
+        }
+    }
+}
