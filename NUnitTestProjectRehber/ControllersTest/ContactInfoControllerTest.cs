e977ddb36a6550ae8c10d741c93209cfdb7af20a
zelaltuncel <zelaltuncel@hotmail.com>
2021-02-17 18:31:59 +0300

    Unit tests are added

---
+++ NUnitTestProjectRehber/ControllersTest/ContactInfoControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Confluent.Kafka;
+using NUnit.Framework;
+using RehberApi.Controllers;
+using RehberApi.Models.ORM.Context;
+using RehberApi.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProjectRehber.ControllersTest
+{
+    class ContactInfoControllerTest
+    {
+        [Test]
+        public void AddContactInfoTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+
+
+            ContactInfoController infoController = new ContactInfoController(_directoryContext);
+
+            ContactAddVM model = new ContactAddVM();
+            model.personID = 1;
+            var result = infoController.Add(model);
+
+            Assert.IsNotNull(result);
+
+
+        }
+
+        [Test]
+        public void DeleteInfoTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+
+            ContactInfoController infoController = new ContactInfoController(_directoryContext);
+            ContactDeleteVM model = new ContactDeleteVM();
+
+            
+            var data = infoController.Delete(model);
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(data);
+
+        }
+
+
+    }
+}
---
+++ NUnitTestProjectRehber/ControllersTest/PersonControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using RehberApi.Controllers;
+using RehberApi.Models.ORM.Context;
+using RehberApi.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NUnitTestProjectRehber.ControllersTest
+{
+    class PersonControllerTests
+    {
+
+        [Test]
+
+        public void GetpeopleListTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+            ProducerConfig _config = new ProducerConfig();
+            PersonController personController = new PersonController(_config, _directoryContext );
+
+            var result = personController.GetPeople();
+
+            if (result != null)
+            {
+                Assert.IsTrue(result.Count > 0);
+            }
+
+        }
+
+        [Test]
+        public void GetDetailTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+            ProducerConfig _config = new ProducerConfig();
+            PersonController personController = new PersonController(_config, _directoryContext);
+
+            var result = personController.GetDetail(2);
+
+            Assert.IsNotNull(result);
+ 
+        }
+
+        [Test]
+        public void AddPersonTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+            ProducerConfig _config = new ProducerConfig();
+            PersonController personController = new PersonController(_config, _directoryContext);
+            PersonAddVM model = new PersonAddVM();
+
+            var result = personController.Add(model);
+
+            Assert.IsNotNull(result);
+
+        }
+
+
+        [Test]
+        public void DeletePersonTest()
+        {
+            DirectoryContext _directoryContext = new DirectoryContext();
+            ProducerConfig _config = new ProducerConfig();
+            PersonController personController = new PersonController(_config, _directoryContext);
+            PersonDeleteVM model = new PersonDeleteVM();
+
+            var data = personController.Delete(model);
+    
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(data);
+        
+          
+        }
+
+
+
+
+
+    }
+}
